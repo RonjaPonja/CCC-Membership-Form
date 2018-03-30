880d5bd0762c32275325797b5b1123df785dc126
Patrick Meyer <patrick.meyer@innogames.com>
2018-03-30 03:09:09 +0200

    Make qr-code decoder a lot more strict

--- qr-decoder.py
+++ qr-decoder.py
@@ -2,25 +2,42 @@
 
 import json
 import base64
+from datetime import datetime
+from typing import Dict, Union
+
 from jsonschema import validate
-from typing import Dict
+
 
 SCHEMA = {
     "type": "object",
     "properties": {
-        "version": {"type": "string"},
-        "payment": {"type": "string"},
-        "firstname": {"type": "string"},
-        "lastname": {"type": "string"},
-        "email": {"type": "string"},
-        "pgp": {"type": "string"},
-        "addr1": {"type": "string"},
-        "addr2": {"type": "string"},
-        "addr3": {"type": "string"},
-        "c/o": {"type": "string"},
-        "country": {"type": "string"},
-        "date": {"type": "string"},
-    }
+        "version": {"type": "string", "pattern": r"^alpha$"},
+        "payment": {"type": "string", "pattern": r"^(regular|reduced)$"},
+        "firstname": {"type": "string", "pattern": r".{1,30}"},
+        "lastname": {"type": "string", "pattern": r".{1,30}"},
+        "email": {"type": "string", "pattern": r".+@.+\..+"},
+        "pgp": {"type": "string", "pattern": r"(0x[a-f0-9]{16}|)"},
+        "addr1": {"type": "string", "pattern": r".{1,30}"},
+        "addr2": {"type": "string", "pattern": r".{1,30}"},
+        "addr3": {"type": "string", "pattern": r".{0,30}"},
+        "country": {"type": "string", "pattern": r"[A-Z]{2}"},
+        "date": {
+            "type": "string",
+            "pattern": r"[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}"
+        },
+    },
+    "required": [
+        "version",
+        "payment",
+        "firstname",
+        "lastname",
+        "email",
+        "pgp",
+        "addr1",
+        "addr2",
+        "country",
+        "date",
+    ]
 }
 
 
@@ -28,14 +45,28 @@ class FormDecodeError(Exception):
     pass
 
 
-def encode_form(form: Dict[str, str]) -> bytes:
+def _encode_form(form: Dict[str, str]) -> bytes:
+    """Generate qr-code content like value for testing."""
     return base64.b64encode(json.dumps(form).encode())
 
 
-def decode_form(form_b64: bytes) -> Dict[str, str]:
+def decode_form(form_b64: bytes) -> Dict[str, Union[str, datetime]]:
+    """Get form data from QRCode.
+
+    This function:
+    - decodes the qr-code bytes as utf8 as we expect base64 anyway,
+    - decodes the utf8 string as base64,
+    - loads the result as json,
+    - validates the resulting object against the modules jsonschema SCHEMA,
+    - and finally tries to convert the date field into a datetime object
+
+    If anything goes wrong a FormDecodeError is raised, otherwise the form
+    data is returned as a dict.
+    """
     try:
         form = json.loads(base64.b64decode(form_b64).decode())
         validate(form, SCHEMA)
+        form['date_parsed'] = datetime.strptime(form['date'], "%d.%m.%Y")
     except Exception as e:
         raise FormDecodeError(
             "Loading or validation of form failed: {}".format(str(e)))
@@ -44,8 +75,9 @@ def decode_form(form_b64: bytes) -> Dict[str, str]:
 
 
 def main():
+    """Demo how to use this lib"""
 
-    form = {
+    test_form = {
         "version": "alpha",
         "payment": "regular",
         "firstname": "Hannah",
@@ -59,12 +91,12 @@ def main():
         "date": "29.3.2018",
     }
 
-    form = decode_form(encode_form(form))
-    print("Valid Form: {}".format(form))
-
     try:
-        form['payment'] = 1
-        form = decode_form(encode_form(form))
+        form = decode_form(_encode_form(test_form))
+        print("Valid Form: {}".format(form))
+
+        test_form['version'] = "foobar"
+        form = decode_form(_encode_form(test_form))
     except FormDecodeError as e:
         print("Invalid Form: {}".format(str(e)))
 
